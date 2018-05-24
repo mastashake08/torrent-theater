78b65d7777a04093444c7bacb4d5ab1270124bff
Jyrone Parker <jyrone.parker@gmail.com>
2018-05-24 01:06:08 -0400

    DEV

--- public/js/app.js
+++ public/js/app.js
@@ -78245,7 +78245,7 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
       client: {},
       torrents: {},
       isReady: false,
-      magnet_uri: '',
+      magnet_uri: 'magnet:?xt=urn:btih:08ada5a7a6183aae1e09d831df6748d566095a10&dn=Sintel&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2F&xs=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel.torrent',
       query: '',
       total: 0
     };
@@ -78282,6 +78282,8 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
           // more. Specify a container element (CSS selector or reference to DOM node).
           if (file.name.endsWith('mp4')) {
             file.renderTo('video#video-player');
+
+            $('#torrentModal').modal('hide');
           } else {}
         });
       });
